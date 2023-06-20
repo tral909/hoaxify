299cacb55559d6e73d0fd8e12b89e09050b2d7e3
tral909 <indorm1992@mail.ru>
2023-06-20 18:03:04 +0300

    Password Hashing

--- src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -48,6 +50,15 @@ public class UserControllerTest {
         User user = createValidUser();
         ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
         assertThat(response.getBody().getMessage()).isNotNull();
+    }
+
+    @Test
+    void postUser_whenUserIsValid_passwordIsHashedInDatabase() {
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
+        List<User> users = userRepository.findAll();
+        User inDB = users.get(0);
+        assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
     }
 
     private User createValidUser() {
