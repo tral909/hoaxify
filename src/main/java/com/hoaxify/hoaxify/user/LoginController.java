15e1c264ce2e4d4bb6b6acc2a6328be4c5e1d364
tral909 <indorm1992@mail.ru>
2023-06-27 13:22:11 +0300

    Authenticated User

--- src/main/java/com/hoaxify/hoaxify/user/LoginController.java
+++ src/main/java/com/hoaxify/hoaxify/user/LoginController.java
@@ -1,13 +1,21 @@
 package com.hoaxify.hoaxify.user;
 
+import com.hoaxify.hoaxify.shared.CurrentUser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class LoginController {
 
     @PostMapping("/api/1.0/login")
-    void handleLogin() {
-
+    Map<String, Object> handleLogin(@CurrentUser User loggedInUser /* main approach */) {
+        // second approach (authentication must be parameter in controller)
+        //User loggedInUser = (User) authentication.getPrincipal();
+        // first approach
+        //User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return Collections.singletonMap("id", loggedInUser.getId());
     }
 }
--- src/test/java/com/hoaxify/hoaxify/LoginControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/LoginControllerTest.java
@@ -1,6 +1,7 @@
 package com.hoaxify.hoaxify;
 
 import com.hoaxify.hoaxify.error.ApiError;
+import com.hoaxify.hoaxify.user.User;
 import com.hoaxify.hoaxify.user.UserRepository;
 import com.hoaxify.hoaxify.user.UserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,10 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -76,6 +81,17 @@ public class LoginControllerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    @Test
+    public void postLogin_withValidCredentials_receiveLoggedInUserId() {
+        User inDB = userService.save(TestUtils.createValidUser());
+        authenticate();
+        ResponseEntity<Map<String, Object>> response = login(new ParameterizedTypeReference<>() {});
+
+        Map<String, Object> body = response.getBody();
+        Integer id = (Integer) body.get("id");
+        assertThat(id).isEqualTo(inDB.getId());
+    }
+
     private void authenticate() {
         testRestTemplate.getRestTemplate().getInterceptors().add(
                 new BasicAuthenticationInterceptor("test-user", "P4ssword"));
@@ -83,5 +99,9 @@ public class LoginControllerTest {
 
     public <T> ResponseEntity<T> login(Class<T> responseType) {
         return testRestTemplate.postForEntity(API_1_0_LOGIN, null, responseType);
+    }
+
+    public <T> ResponseEntity<T> login(ParameterizedTypeReference<T> responseType) {
+        return testRestTemplate.exchange(API_1_0_LOGIN, HttpMethod.POST, null, responseType);
     }
 }
