5683676dbfc6e3a4e2371dbf846c7773daad1b4a
tral909 <indorm1992@mail.ru>
2023-07-08 00:54:51 +0300

    User View Model

--- src/main/java/com/hoaxify/hoaxify/user/LoginController.java
+++ src/main/java/com/hoaxify/hoaxify/user/LoginController.java
@@ -1,21 +1,16 @@
 package com.hoaxify.hoaxify.user;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.hoaxify.hoaxify.shared.CurrentUser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Map;
 
 @RestController
 public class LoginController {
 
     @PostMapping("/api/1.0/login")
-    Map<String, Object> handleLogin(@CurrentUser User loggedInUser /* main approach */) {
-        // second approach (authentication must be parameter in controller)
-        //User loggedInUser = (User) authentication.getPrincipal();
-        // first approach
-        //User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return Collections.singletonMap("id", loggedInUser.getId());
+    @JsonView(Views.Base.class)
+    User handleLogin(@CurrentUser User loggedInUser) {
+        return loggedInUser;
     }
 }
--- src/main/java/com/hoaxify/hoaxify/user/User.java
+++ src/main/java/com/hoaxify/hoaxify/user/User.java
@@ -1,5 +1,6 @@
 package com.hoaxify.hoaxify.user;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -19,15 +20,18 @@ import java.util.Collection;
 public class User implements UserDetails {
     @Id
     @GeneratedValue
+    @JsonView(Views.Base.class)
     private long id;
 
     @NotNull(message = "{hoaxify.constraints.username.NotNull.message}")
     @Size(min = 4, max = 255)
     @UniqueUsername
+    @JsonView(Views.Base.class)
     private String username;
 
     @NotNull
     @Size(min = 4, max = 255)
+    @JsonView(Views.Base.class)
     private String displayName;
 
     @NotNull
@@ -35,6 +39,9 @@ public class User implements UserDetails {
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",
             message = "{hoaxify.constraints.password.Pattern.message}")
     private String password;
+
+    @JsonView(Views.Base.class)
+    private String image;
 
     @Override
     @Transient
