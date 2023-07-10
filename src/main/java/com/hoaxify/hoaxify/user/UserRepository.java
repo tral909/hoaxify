f85ea012dfa99a84b395964973ebd0dc095d7fb8
tral909 <indorm1992@mail.ru>
2023-07-10 10:29:35 +0300

    User Modelling Projection

--- src/main/java/com/hoaxify/hoaxify/user/UserRepository.java
+++ src/main/java/com/hoaxify/hoaxify/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.hoaxify.hoaxify.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
     List<User> findByUsernameContaining(String username);
+
+    @Query(value = "select * from users", nativeQuery = true)
+    Page<UserProjection> getAllUsersProjection(Pageable pageable);
 }
--- src/main/java/com/hoaxify/hoaxify/user/UserService.java
+++ src/main/java/com/hoaxify/hoaxify/user/UserService.java
@@ -29,6 +29,6 @@ public class UserService {
 
     public Page<?> getUsers() {
         Pageable pageable = PageRequest.of(0, 10);
-        return userRepository.findAll(pageable);
+        return userRepository.getAllUsersProjection(pageable);
     }
 }
