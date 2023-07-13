5ab83227a259290c53d4797edc06c6697828c7de
tral909 <indorm1992@mail.ru>
2023-07-13 14:05:01 +0300

    Created Random Users

--- src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
+++ src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
@@ -1,13 +1,35 @@
 package com.hoaxify.hoaxify;
 
+import com.hoaxify.hoaxify.user.User;
+import com.hoaxify.hoaxify.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class HoaxifyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HoaxifyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HoaxifyApplication.class, args);
+    }
 
+    @Bean
+    @Profile("!test")
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            IntStream.rangeClosed(1, 15)
+                    .mapToObj(i -> {
+                        User user = new User();
+                        user.setUsername("user" + i);
+                        user.setDisplayName("display" + i);
+                        user.setPassword("P4ssword");
+                        return user;
+                    })
+                    .forEach(userService::save);
+        };
+    }
 }
